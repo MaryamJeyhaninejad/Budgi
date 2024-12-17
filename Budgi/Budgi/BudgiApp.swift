2d473cf954805d289744a0a23eb88da8ec83a08b
MaryamJeyhaninejad <Maryamjeyhani75@gmail.com>
2024-12-17 09:35:59 +0100

    first commit

---
+++ Budgi/Budgi/BudgiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgiApp.swift
+//  Budgi
+//
+//  Created by Maryam Jeyhaninejad on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BudgiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
